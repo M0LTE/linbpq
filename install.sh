fda49d9d28b8654edbd85b8997eb44c82c3806a9
Ubuntu <ubuntu@m0lte-node.lan>
2021-12-01 19:39:57 +0000

    Work on install

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -e
 
+systemctl stop linbpq || true
+systemctl disable linbpq || true
+
+rm -f /usr/local/bin/linbpq
 ln -s ~/linbpq /usr/local/bin/linbpq
+
 ln -s /usr/local/bin/linbpq/linbpq.service /etc/systemd/system/linbpq.service
 systemctl daemon-reload
 systemctl enable linbpq.service
